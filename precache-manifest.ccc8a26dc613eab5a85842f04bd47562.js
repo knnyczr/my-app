6a9e37b229bb542527c85fa9145271c81208db09
KennyCruzer <kennycruz@Kenneths-MBP.fios-router.home>
2020-03-25 10:21:25 -0400

    Updates

---
+++ precache-manifest.ccc8a26dc613eab5a85842f04bd47562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb717ba671e940d9d06537041509e8af",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "bd25be0d0ceabd381a7e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/my-app/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd25be0d0ceabd381a7e",
+    "url": "/my-app/static/js/main.698b5f86.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
